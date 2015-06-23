8cabacb0aa977ed4cc41f7a7a5abe79986037210
Will Schenk <wschenk@gmail.com>
2015-06-23 09:38:23 -0400

    Merge fixes

--- lib/middleman-react/extension.rb
+++ lib/middleman-react/extension.rb
@@ -15,6 +15,7 @@ module Middleman
       option :components, ['components.js'], 'List of files for precompiling'
       option :replay_console, true, 'Replay console for prerendered components'
 
+      # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
       def initialize(app, options_hash = {}, &block)
         super
 
@@ -23,12 +24,18 @@ module Middleman
         Middleman::React::CJSX::Template.harmony = options[:harmony]
         Middleman::React::CJSX::Template.strip_types = options[:strip_types]
 
+        Middleman::React::Renderer.components = options[:components]
+        Middleman::React::Renderer.replay_console = options[:replay_console]
+
         ::Tilt.register 'jsx', Middleman::React::JSX::Template
         ::Sprockets.register_engine 'jsx', Middleman::React::JSX::Template
 
         ::Tilt.register 'cjsx', Middleman::React::CJSX::Template
         ::Sprockets.register_engine 'cjsx', Middleman::React::CJSX::Template
+
+        app.helpers Middleman::React::Helpers
       end
+      # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
       def after_configuration
         @app.template_extensions cjsx: :js, jsx: :js
