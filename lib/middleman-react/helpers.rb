d3ff19aca3a104efa6224b2454218bd2860a71a5
Leon Daniel Thorne <ldthorne96@gmail.com>
2017-04-26 19:36:03 -0400

    Remove wrapping each element in container

--- lib/middleman-react/helpers.rb
+++ lib/middleman-react/helpers.rb
@@ -6,27 +6,7 @@ module Middleman
     module Helpers
       # rubocop:disable Metrics/MethodLength
       def react_component(name, args = {}, options = {}, &block)
-        options = { tag: options } if options.is_a?(Symbol)
-        content = ''
-
-        if options[:prerender]
-          content = Middleman::React::Renderer.render(self, name, args)
-        end
-
-        html_options = options.reverse_merge(data: {})
-        html_options[:data].tap do |data|
-          data[:react_class] = name
-          unless args.empty?
-            data[:react_props] = Middleman::React::Renderer.react_props(args)
-          end
-        end
-
-        html_tag = html_options[:tag] || :div
-
-        # remove internally used properties so they aren't rendered to DOM
-        html_options.except!(:tag, :prerender)
-
-        content_tag(html_tag, content, html_options, &block)
+        content = Middleman::React::Renderer.render(self, name, args)
       end
     end
   end
